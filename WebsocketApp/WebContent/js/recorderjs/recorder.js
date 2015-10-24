db25feea231e65132066e6e50cbeff94aae6d300
siegenth <martin@siegenthaler.org>
2015-10-24 16:29:07 -0700

    First commit

---
+++ WebsocketApp/WebContent/js/recorderjs/recorder.js
@@ -0,0 +1,206 @@
+/*License (MIT)
+
+Copyright Â© 2013 Matt Diamond
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of 
+the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
+THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+DEALINGS IN THE SOFTWARE.
+*/
+
+(function(window){
+
+  var AUDIO_WORKER_PATH =   'js/recorderjs/recorderWorker.js';
+  var WS_WORKER_PATH = 'js/recorderjs/wsWorker.js';
+
+  var Recorder = function(source, cfg){
+    var config = cfg || {};
+    var bufferLen = config.bufferLen || 4096;
+    this.context = source.context;
+    if(!this.context.createScriptProcessor){
+       this.node = this.context.createJavaScriptNode(bufferLen, 2, 2);
+    } else {
+       this.node = this.context.createScriptProcessor(bufferLen, 2, 2);
+    }
+
+    // Setup WS thread'
+    // TODO get the IP address from web context, Voci does this
+    var wsWorker ;
+    if ("WebSocket" in window) {
+       var nodePort;                  // not debugging IP
+      if (!nodePort) {
+        nodePort = window.document.location.host;
+      }
+      var webSocketForwarderUrl  = nodePort + "/WebsocketApp/Forward";
+
+      console.log("Connection using webSocketForwarderUrl:" + webSocketForwarderUrl);
+      console.log("Connection using SessionId:" + SessionId);      
+      wsWorker = new Worker(WS_WORKER_PATH);
+      wsWorker.postMessage({
+        command: 'init',
+        config: {
+          url: webSocketForwarderUrl,
+          senderId: SessionId,
+          audioType: "audio/wav",
+          chunkSize: 1000
+        }
+      });
+      console.log("setup wsWorker");
+    }
+    /// setup Audio thread
+    var audioWorker = new Worker(config.workerPath || AUDIO_WORKER_PATH);
+    audioWorker.postMessage({
+      command: 'init',
+      config: {
+        sampleRate: this.context.sampleRate
+      }
+    });
+    var recording = false, wsTransmitting = false,
+      currCallback, wsCallback;
+
+    this.node.onaudioprocess = function(e){
+      if (!recording) return;
+      if (wsTransmitting) {
+//        console.log("recording:" + e.inputBuffer.getChannelData(0).length)
+        wsWorker.postMessage({
+          command: "chunk",
+          chunkBuffer: [e.inputBuffer.getChannelData(0)]
+        });
+      }
+      audioWorker.postMessage({
+        command: 'record',
+        buffer: [
+          e.inputBuffer.getChannelData(0),
+          e.inputBuffer.getChannelData(1)
+        ]
+      });
+    }
+  // TODO - what is this mapping?
+    this.configure = function(cfg){
+      for (var prop in cfg){
+        if (cfg.hasOwnProperty(prop)){
+          config[prop] = cfg[prop];
+        }
+      }
+    }
+
+
+    // send the data to Streams via webSockets
+    this.wsTransmit = function(buffer){
+      console.log("wsTransmit@recorder:" + buffer.length);
+      wsWorker.postMessage({
+        command: 'transmit',
+        transmitBuffer: buffer
+      });
+    }
+	console.log(document.getElementById("StreamsIP").value + "@recorder.js");
+
+    this.wsStartChunking = function() {
+    	console.log("start chunking");
+      wsWorker.postMessage({
+        command: "startChunking", 
+        streamsIP: document.getElementById("StreamsIP").value,
+        senderId: document.getElementById("StreamsID").value                
+      });
+      wsTransmitting = true;
+    }
+
+    this.wsStopChunking = function() {
+
+      wsWorker.postMessage({
+          command:"stopChunking"
+      });
+      wsTransmitting = false;
+    }
+
+    this.record = function(){
+      recording = true;
+    }
+
+    this.stop = function(){
+      recording = false;
+    }
+
+    this.clear = function(){
+      audioWorker.postMessage({ command: 'clear' });
+    }
+
+    this.getBuffers = function(cb) {
+      currCallback = cb || config.callback;
+      audioWorker.postMessage({ command: 'getBuffers' });
+    }
+
+    this.exportWAV = function(cb, type){
+      currCallback = cb || config.callback;
+      type = type || config.type || 'audio/wav';
+      if (!currCallback) throw new Error('Callback not set');
+      audioWorker.postMessage({
+        command: 'exportWAV',
+        type: type
+      });
+    }
+
+    this.exportMonoWAV = function(cb, type){
+      currCallback = cb || config.callback;
+      type = type || config.type || 'audio/wav';
+      if (!currCallback) throw new Error('Callback not set');
+      audioWorker.postMessage({
+        command: 'exportMonoWAV',
+        type: type
+      });
+    }
+
+    this.exportSocketWAV = function(cb, type) {
+      currCallback = cb || config.callback;
+      type = type || config.type || 'audio/wav';
+      if (!currCallback) throw new Error('Callback not set');
+      audioWorker.postMessage({
+        command: 'exportSocketWAV',
+        type: type
+      });
+    }
+
+    this.sendWebSocket = function(cb, type) {
+      currCallBack = cb;
+      wsWorker.postMessage({
+        command: 'transmit',
+        size: 1234
+      });
+    }
+
+
+    audioWorker.onmessage = function(e){   // This is where the data is returned.
+      var blob = e.data;
+      currCallback(blob);
+    }
+
+
+    wsWorker.onmessage = function(e){   // This is where the data is returned.
+      var command = e.data;
+      console.log("wsWorker.onmessage : " + command);
+      wsCallback && wsCallback(command);
+    }
+
+    source.connect(this.node);
+    this.node.connect(this.context.destination);   // if the script node is not connected to an output the "onaudioprocess" event is not triggered in chrome.
+  };// End of the Recorder object definition
+
+  Recorder.setupDownload = function(blob, filename) {
+    var url = (window.URL || window.webkitURL).createObjectURL(blob);
+    var link = document.getElementById("save");
+    link.href = url;
+    link.download = filename || 'output.wav';
+  }
+
+  window.Recorder = Recorder;
+
+})(window)
---
+++ WebsocketApp/WebContent/js/recorderjs/wsWorker.js
@@ -0,0 +1,172 @@
+/*License (MIT)
+
+ Send data out over websocket interface.
+    - Sends whole file (many buffers)
+    - Streams live data out, one buffer at time.
+
+*/
+// TODO * need to state what type of data is being sent.
+// TODO * init should have type - and any other constant
+// TODO * we have two flavors of control, not good. It starts with '<' or with a '#'.
+
+var recLength = 0,
+    recBuffersL = [],
+    recBuffersR = [],
+    wsUrl,
+    senderId,
+    audioType,
+    wsSnd,
+    window;
+
+this.onmessage = function(e){
+  console.log("wsWorker dispatcher:" + e.data.command);
+  switch(e.data.command){
+    case 'init':
+      init(e.data.config);
+      break;
+    case 'transmit':
+      transmit(e.data.transmitBuffer);
+      break;
+    // in realtime send chunk of data
+    case 'chunk':
+      transmitChunk(e.data.chunkBuffer);
+      break;
+    case 'startChunking' :
+      senderId = e.data.senderId.replace('#','_');    	
+      transmitCommand("START", e.data.streamsIP); // IPAddress of Streams instance. 
+      break;
+    case 'stopChunking' :
+      transmitCommand("STOP");
+      break;
+    case 'close':
+      break;
+  }
+};
+/**
+ * Init
+ *   - setup the WS connection.
+ *   - store the id, cannot use the '#' remove it.
+ * @param config
+ */
+
+function init(config) {
+  console.log("recorderWorker - init");
+  senderId = config.senderId.replace('#','_');
+  audioType = config.audioType;
+
+  var wsSoc = "ws://" + config.url;
+
+  // connection to Streams
+  console.log("Connecting to : " + wsSoc);
+  wsSnd = new WebSocket(wsSoc);
+  wsSnd.onopen = function () {
+    // Web Socket is connected, send data using send()
+    wsSnd.send("<CONTROL size=\"1024\">BINARY</CONTROL>");
+    console.log("WS control sent");
+  };
+
+  wsSnd.onmessage = function (evt) {
+    var received_msg = evt.data;
+    //sndMsg.innerHTML = "Alert:<b>" + received_msg + "</b>";
+  };
+  wsSnd.onclose = function () {
+    //sndMsg.innerHTML = "Connection closed....";
+    wsSnd = null;
+  };
+  // setInterval(buildMessage, 100);
+}
+
+
+
+function buildMessage(command, message) {
+	console.log("buildMessage");
+  return ("#" + senderId + "#" + command + "#" + (message || ""));
+}
+
+function transmitCommand(command, message) {
+  wsSnd.send(buildMessage(command,message));
+}
+
+// TODO Send header state what we are sending - this may need the size
+function transmit(fullBuffer) {
+  wsSnd.send("<DATALENGTH>" + fullBuffer.length + "</DATALENGTH>");
+  var siz = 2048;
+  var idx, tmpBuf;
+  for (idx=0; idx <= fullBuffer.length; idx += siz) {
+    tmpBuf = fullBuffer.slice(idx, idx+siz);
+    wsSnd.send(tmpBuf);
+  }
+  if (idx != fullBuffer.length) {
+    tmpBuf = fullBuffer.slice(idx, fullBuffer.length);
+    wsSnd.send(tmpBuf);
+  }
+  wsSnd.send("<END></END>");
+  console.log("TRANSMIT START");
+}
+
+/**
+ * Convert the outbound audio data to ascii text, add a header
+ * to beginning of each message. You need this on the relieving
+ * side so it can keep track of he Audio source coming in.
+ *
+ *
+ * @param audioBlob
+ */
+
+function transmitAudioBytes(audioBlob) {
+  var myReader = new FileReaderSync();
+  myReader.addEventListener("loadend", function(e) {
+    //console.log(e.srcElement.result);
+    var arrayBuffer = e.target.result;
+    // TODO - move DataView into worker thread
+    var binaryData = buildMessage("DATA", "");
+    var view = new DataView(arrayBuffer);
+    var viewLen = view.byteLength;
+    // TODO - move convert into the worker thread.
+    for (var i = 0; i < viewLen; binaryData += ("00" + view.getUint8(i++).toString(16)).substr(-2).toUpperCase());
+    console.log("socketSend@main " + binaryData.length);
+    wsSnd.send(binaryData);
+  });
+  myReader.readAsArrayBuffer(audioBlob);
+  //myReader.readAsBinaryString(sendBlob);
+}
+
+
+function floatTo16BitPCM(output, offset, input){
+  for (var i = 0; i < input.length; i++, offset+=2){
+    var s = Math.max(-1, Math.min(1, input[i]));
+    output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true);
+  }
+}
+
+
+function encodeChunk(samples, mono){
+  var buffer = new ArrayBuffer(samples.length * 2);
+  var view = new DataView(buffer);
+
+  // We are not sending the wav header, it is the responsiblity of the
+  // receiver to build the .wav header since we do not know how much
+  // Refer to the wavBuild script file that add on header.
+
+
+  floatTo16BitPCM(view, 0, samples);
+
+  return view;  // this is where the audio file is setup. can we push this to streams here?
+                // we have multiple saves, where are the others happening.
+}
+
+function mergeFloat(recBuffers, recLength){
+  var result = new Float32Array(recLength);
+  result.set(recBuffers, 0);
+  return result;
+}
+// TODO Send a chunk
+function transmitChunk(chunkBuffer) {
+  var bufFloat = mergeFloat(chunkBuffer[0], chunkBuffer[0].length);
+  var dataview = encodeChunk(bufFloat, true);
+  var audioBlob = new Blob([dataview], { type: audioType });
+  transmitAudioBytes(audioBlob);
+  this.postMessage("next");
+}
+
+
