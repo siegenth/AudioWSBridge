f63609c6dfb5f2f6dcc6d0a1e247b838929061d9
siegenth <martin@siegenthaler.org>
2015-11-06 11:40:11 -0800

    test if a change can get pushed all the way up

--- WebsocketApp/WebContent/COMMON.js
+++ WebsocketApp/WebContent/COMMON.js
@@ -1,9 +1,11 @@
 /**
  * Common point to pick up the Streams IP address. 
+ * This is set on a per instance basis, I have
+ * an local instance and a BlueMix instance. 
  */
 // local development
 var COMMON_FIXIP="172.16.49.152";
-// external
+// external - public
 //var COMMON_FIXIP="169.55.246.122";
-// remote
+// external - private 
 //var COMMON_FIXIP="xxx.xx.xxx.xxx";
