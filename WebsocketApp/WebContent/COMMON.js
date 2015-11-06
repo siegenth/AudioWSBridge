402cc6225f8394fbeaf0fe810f923563e30ce075
siegenth <martin@siegenthaler.org>
2015-11-06 11:21:58 -0800

    Update documentation

--- WebsocketApp/WebContent/COMMON.js
+++ WebsocketApp/WebContent/COMMON.js
@@ -1,5 +1,9 @@
 /**
  * Common point to pick up the Streams IP address. 
  */
+// local development
 var COMMON_FIXIP="172.16.49.152";
+// external
 //var COMMON_FIXIP="169.55.246.122";
+// remote
+//var COMMON_FIXIP="xxx.xx.xxx.xxx";
