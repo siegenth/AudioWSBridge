d66eef3ac537245ced45ef7b90a8c163d8124a66
siegenth <martin@siegenthaler.org>
2015-11-09 11:17:56 -0800

    Snap of currently running.

--- WebsocketApp/WebContent/COMMON.js
+++ WebsocketApp/WebContent/COMMON.js
@@ -4,8 +4,13 @@
  * an local instance and a BlueMix instance. 
  */
 // local development
-var COMMON_FIXIP="172.16.49.152";
+//var COMMON_FIXIP="172.16.49.152";
+//var COMMON_FIXIP="172.16.49.153";
+//var COMMON_FIXIP="172.16.49.154";
 // external - public
-//var COMMON_FIXIP="169.55.246.122";
+var COMMON_FIXIP="169.55.246.122"; // .213 
+//var COMMON_FIXIP="169.45.142.238"; // .166
 // external - private 
 //var COMMON_FIXIP="xxx.xx.xxx.xxx";
+//TextCell["\"10.142.241.166\"->\"169.45.142.238\""]
+//TextCell["\"10.143.133.213\"->\"169.55.246.122\""]
